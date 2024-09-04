b3def48bba3a30759be5e5783e340374e2cfe37b
Hest <hestdatabase@gmail.com>
2024-09-04 18:51:34 -0300

    Estilos

--- cliente.js
+++ cliente.js
@@ -1,18 +1,46 @@
-const socket = new WebSocket("wss://df36c15c-9bf4-499f-9fc1-37d3a0d51872-00-n0hngb84iljj.worf.replit.dev/")
+const socket = new WebSocket("http://localhost:8080")
 
 socket.onopen = () => {
   console.log("Conexão com o WebSocket estabelecida.")
 }
 
 socket.onmessage = (event) => {
-  let chat = document.querySelector("#chat")
-  let mensagem = document.createElement("div")
-  event.data.text().then(msg => mensagem.innerText = msg)
-  chat.appendChild(mensagem)
+  let data;
+  event.data.json().then(data => data = data)
+  if (data.sender != "self") {
+    let chat = document.querySelector("#chat")
+    let mensagem = document.createElement("div")
+    mensagem.classList.add("mensagem")
+    
+    event.data.text().then(msg => mensagem.innerText = msg)
+    chat.appendChild(mensagem)
+  }
 }
 
 function enviarMensagem() {
-  let mensagem = document.querySelector("#mensagem")
-  socket.send(mensagem.value)
-  mensagem.value = ""
+  let input = document.querySelector("#input")
+  let chat = document.querySelector("#chat")
+  let mensagem = document.createElement("div")
+  mensagem.classList.add("mensagem", "enviada")
+  mensagem.innerText = input.value
+  chat.appendChild(mensagem)
+  
+  socket.send(JSON.stringify({
+    mensagem: input.value,
+    sender: "self"
+  }))
+  
+  input.value = ""
+}
+
+
+function mostrarEnviar() {
+  let input = document.querySelector("#input")
+  let enviar = document.querySelector("#enviar")
+  
+  if (input.value) {
+    enviar.style.display = "inline-block"
+  } else {
+    enviar.style.display = "none"
+  }
 }
